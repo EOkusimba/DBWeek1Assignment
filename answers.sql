13291a2a222f922702217416000370aff7268976
EOkusimba <okusimbaopindi@gmail.com>
2025-03-21 15:39:24 +0300

    Add files via upload

---
+++ answers.sql
@@ -0,0 +1,12 @@
+-- Question 1: Write an SQL query to create a new database called salesDB.
+-- Creation of salesDB database
+create database salesDB;
+
+-- Question 2: Write an SQL query to drop (delete) the database called demo.
+-- Creation of demo database
+create database demo;
+
+-- Deletion of demo database
+drop database demo;
+
+
